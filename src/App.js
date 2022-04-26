e274df8e58abf3a3517dedbf77aeb50432dd87ea
juliansilveira <julian.silveira.annoni@gmail.com>
2022-04-26 19:27:05 -0300

    subida de calculadora

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [currentOperation, setCurrentOperation] = useState('');
+  const [result, setResult] = useState(0);
+
+  function limpiarTodo() {
+    setNumber1('');
+    setNumber2('');
+    setCurrentOperation('');
+    setResult('');
+  }
+
+  function clickNumber(val) {
+    if (currentOperation === '') {
+      setNumber1(number1 + val);
+    } else {
+      setNumber2(number2 + val);
+    }
+  }
+  function clickOperation(val) {
+    setCurrentOperation(val);
+  }
+
+  function getResult() {
+    switch (currentOperation) {
+      case '+':
+        setResult(Number(number1) + Number(number2));
+        break;
+      case '-':
+        setResult(Number(number1) - Number(number2));
+        break;
+      case '*':
+        setResult(Number(number1) * Number(number2));
+        break;
+      case '/':
+        setResult(Number(number1) / Number(number2));
+        break;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculadora En React</h1>
+      <div className='calculadora-grid'>
+        <div className='output'>
+          <div className='operando-anterior'> {currentOperation ? number1 + currentOperation : ''} </div>
+          <div className='operando-actual'> {result ? result : (!currentOperation ? number1 : number2)} </div>
+        </div>
+        <button onClick={() => { limpiarTodo() }} className='span-two'>AC</button>
+        <button onClick={() => { }}>DEL</button>
+        <button onClick={() => { clickOperation('/') }}>/</button>
+        <button onClick={() => { clickNumber(7) }}>7</button>
+        <button onClick={() => { clickNumber(8) }}>8</button>
+        <button onClick={() => { clickNumber(9) }}>9</button>
+        <button onClick={() => { clickOperation('*') }}>*</button>
+        <button onClick={() => { clickNumber(4) }}>4</button>
+        <button onClick={() => { clickNumber(5) }}>5</button>
+        <button onClick={() => { clickNumber(6) }}>6</button>
+        <button onClick={() => { clickOperation('+') }}>+</button>
+        <button onClick={() => { clickNumber(1) }}>1</button>
+        <button onClick={() => { clickNumber(2) }}>2</button>
+        <button onClick={() => { clickNumber(3) }}>3</button>
+        <button onClick={() => { clickOperation('-') }}>-</button>
+        <button onClick={() => { }}>.</button>
+        <button onClick={() => { clickNumber(0) }}>0</button>
+        <button onClick={getResult} className='span-two'>=</button>
+      </div>
     </div>
   );
 }
